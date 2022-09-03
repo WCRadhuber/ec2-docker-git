095fadf85ad745e33b48cdfc83ac7e56c2f80753
WCRadhuber <WCRadhuber@gmail.com>
2022-09-02 20:09:29 -0400

    Script for EC2 adding docker, and git

---
+++ ec2entry.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+yum update -y
+sudo yum install docker -y
+sudo usermod -a -G docker ec2-user
+id ec2-user
+newgrp docker
+wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)
+sudo mv docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
+sudo chmod -v +x /usr/local/bin/docker-compose
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+sudo yum install git -y
